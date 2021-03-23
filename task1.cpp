e1995d62e2bfbf0c87ca1155bfb5905a04b484ba
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-03-23 21:06:03 +0300

    using iterators

--- task1.cpp
+++ task1.cpp
@@ -1,31 +1,57 @@
-#include<fstream>
-#include<string>
-#include<iostream>
-#include<regex>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <fstream>
+
 int main()
 {
     using namespace std;
+    fstream fin("test.txt", ios::in);
 
-    ifstream in("test.txt");
-    if(!in.is_open()){
-        cerr<<"Can't open \"test.txt\"!";
-        exit(EXIT_FAILURE);
+    string text{
+            istreambuf_iterator<char>(fin), istreambuf_iterator<char>() };
+    string result;
+    string::iterator count=text.begin(), first=text.begin(), last=text.begin();
+    fin.close();
+    while (count < text.end()-2) {
+        if (*count == '"' && *(count-1)!='\\') {
+            bool i = false;
+            while (!i) {
+                count = find(count + 1, text.end(), '"');
+                if (*(count-1 ) == '\\') {
+                }
+                else
+                    i = true;
+            }
+            count++;
+        }
+
+        if (*count == '/' && *(count + 1) == '/') {
+            last = count;
+            result.append(first, last);
+            first = find(count, text.end(), '\n');
+            count = first;
+        }
+        if (*count == '/' && *(count + 1) == '*') {
+            last = count;
+            result.append(first, last);
+            count += 2;
+            {
+                bool i = false;
+                while (!i && count<text.end()-1) {
+                    count = find(count, text.end(), '*');
+                    if (*(count + 1) == '/') {
+                        first = count+2;
+                        i = true;
+                    }
+                    else count++;
+                }
+            }
+
+        }
+        else  count++;
     }
-
-    string file, tmp;
-    while(getline(in, tmp))
-    {
-        file+=tmp;
-        file+="\n";
-    }
-    regex pattern(R"(([\/][\/]([[:alnum:]]|[+=<>#,._();"'{}[\]\/\*\\ ]){0,}\n|\r)|([\/][\*]([[:alnum:]]|[+=<>#,._();"'{}[\]\/\\ ]|\n|\r|\t){0,}[\*][\/]))");
-    file = regex_replace(file, pattern, "\n");
-    regex pattern1(R"([\n]{1,})");
-    file = regex_replace(file, pattern1, "\n");
-
-    in.close();
-    ofstream out("test.txt");
-    out<<file;
-    out.close();
-    return 0;
+    fstream fout("test.txt", ios::out);
+    fout<< result;
 }
